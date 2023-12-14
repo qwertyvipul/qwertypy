f6eede4a8641742bc9fd404e18a3c3df6ce344f1
/ | | |_| |_ <vipuls526@gmail.com>
2023-12-14 23:37:29 +0530

    Released version 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='qwertypy',
-    version='0.5.1',
+    version='0.5.2',
     author='Vipul Sharma',
     description='My personal utilities library.',
     long_description=long_description,
